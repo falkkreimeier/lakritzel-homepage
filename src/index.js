0de6b2baead8e0c9001e42f043b4fbde3f7d4844
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-03 13:01:28 +0200

    initialise App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
+import GlobalStyles from "./components/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
